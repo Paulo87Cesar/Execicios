898c6fbbfad0e3034c95f5602bc3d4a65d61c4a6
Paulo Cesar de Oliveira <pc30rock@gmail.com>
2025-01-14 16:30:15 -0300

    Feito execicio 3 primeira parte

---
+++ EX-3/script.js
@@ -0,0 +1,25 @@
+function calcular() {
+    let carro = (document.getElementById('carro').value);
+    let velocidade = parseFloat(document.getElementById('velocidade').value);
+
+    let carro2 = (document.getElementById('carro2').value);
+    let velocidade2 = parseFloat(document.getElementById('velocidade2').value);
+
+    if (isNaN(velocidade) || isNaN(velocidade2)) {
+        document.getElementById('resultado').innerText = 'Por favor, insira valores válidos.';
+        return;
+    }
+    if (velocidade > velocidade2){
+        document.getElementById('resultado').innerHTML = `
+     O carro ${carro} é mais veloz!`
+    }else if(velocidade < velocidade2){
+              document.getElementById('resultado').innerHTML = `
+     O carro ${carro2} é mais veloz!`
+    }else {
+              document.getElementById('resultado').innerHTML = `
+     O carro ${carro} e o ${carro2} tem a mesma velocidade!`
+    }
+    
+}
+
+
